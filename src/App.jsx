95e60f3d32195501a320dd5af2eaa47730ae801a
omer <jagenau49@gmail.com>
2022-06-21 17:32:33 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Search in API Page</h1>
+      <div className="search-container">
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
