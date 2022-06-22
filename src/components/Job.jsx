e16e16b02fb6783c0fbf49fb95ef04ce0e03821f
omer <jagenau49@gmail.com>
2022-06-22 10:37:06 +0200

    job's html view added

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,9 +1,12 @@
 export const Job = ({ item }) => {
-  //TODO:show html content
+  
   return (
     <div className="jobBox">
       <h3>{item.idCode}</h3>
-      <pre>{item.html}</pre>
+      <div
+        className="jobDetails"
+        dangerouslySetInnerHTML={{ __html: item.html }}
+      ></div>
     </div>
   );
 };
